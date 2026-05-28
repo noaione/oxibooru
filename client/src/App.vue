1a02e66567419ce3a400543ed913952e886a368f
noaione <noaione0809@gmail.com>
2026-05-28 12:05:38 +0700

    client/fix: issue with settings + delete missing

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
 import { RouterView } from 'vue-router';
 import NavBar from './components/NavBar.vue';
 import ToastNotification from './components/ToastNotification.vue';
 import PageLoader from './components/PageLoader.vue';
 import ConfirmDialog from './components/ConfirmDialog.vue';
+import { useSettingsStore, useDarkTheme } from './stores/settings.ts';
+
+const darkMode = useDarkTheme();
+const settings = useSettingsStore();
+
+onMounted(() => {
+  darkMode.init();
+  settings.init();
+});
 </script>
 
 <template>
