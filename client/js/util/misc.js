68a76ebe3774312ab676f72f0198c241c0391282
noaione <noaione0809@gmail.com>
2026-05-28 07:55:59 +0700

    client/feat: nicer bytes size

--- client/js/util/misc.js
+++ client/js/util/misc.js
@@ -46,10 +46,10 @@ function formatFileSize(fileSize) {
     return _formatUnits(
         fileSize,
         1024,
-        ["B", "K", "M", "G"],
+        ["B", "KiB", "MiB", "GiB", "TiB"],
         (number, suffix) => {
-            const decimalPlaces = number < 20 && suffix !== "B" ? 1 : 0;
-            return number.toFixed(decimalPlaces) + suffix;
+            const decimalPlaces = suffix !== "B" ? 2 : 0;
+            return number.toFixed(decimalPlaces) + ' ' + suffix;
         }
     );
 }
